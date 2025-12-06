0f2a7d67764f14bc6dc84fe7f818e2f941ad795e
Malte <132433059+MaeschDEV@users.noreply.github.com>
2025-12-07 00:07:19 +0100

    Feature/fix text overflow on smartphone (#4)
    * Fixed the timeline to not overflow anymore.
    
    * Fixed the text in work section to not overflow anymore.
    
    * Fixed Project section padding to match the rest.
    
    ---------
    
    Co-authored-by: Malte Esch <malteesch1508@gmail.com>

--- app/components/projects.tsx
+++ app/components/projects.tsx
@@ -4,7 +4,7 @@ import ProjectCard from "./projectCard";
 
 const projects = () => {
   return (
-    <div className="p-16">
+    <div className="p-10">
       <h1 className="mb-5 text-2xl font-semibold text-left">Projects</h1>
       <ProjectCard></ProjectCard>
     </div>
